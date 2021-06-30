31db4a7e903a5228bef5ec2643da3bf7ea5ac5a8
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-06-29 21:13:07 -0300

    implements layout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,30 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset('images/PngItem_5088909.png'),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: [
+          Align(
+            alignment: Alignment.center,
+            child: Text('0'),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.star),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
