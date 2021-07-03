db9cdc344d39e64d93747957b07dffccc6f7e5c5
Rafa-Oli <rafaela.albaniza@gmail.com>
2021-07-03 17:39:23 -0300

    implements blocs

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 // @dart=2.9
+import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertube/api.dart';
 import 'package:fluttertube/screens/home.dart';
+
+import 'blocs/videos_bloc.dart';
 
 void main() {
   Api api = Api();
@@ -12,9 +15,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FlutteTube',
-      home: Home(),
-    );
+    return BlocProvider(
+        blocs: [Bloc((i) => VideosBloc())],
+        dependencies: [],
+        child: MaterialApp(
+          title: 'FlutteTube',
+          home: Home(),
+        ));
   }
 }
